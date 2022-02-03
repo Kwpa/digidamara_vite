0ab499ca7e7f270a44da677262638bdb8690e3b8
Kwpa <kai.mcgilligan.oliver@gmail.com>
2022-02-03 17:22:49 -0400

    [Kai] Camera icon added to test

--- src/scenes/elements/BaseWebsite.jsx
+++ src/scenes/elements/BaseWebsite.jsx
@@ -7,13 +7,9 @@ const BaseWebsite = () => {
         <div class='notification has-background-dark'>
           <nav class="level is-mobile">
             <div class="level-item has-text-centered">
-              <button class="button">
-                <span title="Badge top right" class="badge">8</span>
-                Button
-              </button>
               <span>
                 <h1 class="heading has-text-centered has-text-white">Round</h1>
-                <img class="team" src="/assets/icon_day.png"></img>
+                <img class="hdrftr-icon" src="/assets/icon_day.png"></img>
               </span>
               <span>
                 <h1 class="title is-size-2 has-text-white">1</h1>
@@ -22,7 +18,7 @@ const BaseWebsite = () => {
             <div class="level-item has-text-centered">
               <span>
                 <h1 class="heading has-text-centered has-text-white">Action Points</h1>
-                <img class="team" src="/assets/icon_actionpoints.png"></img>
+                <img class="hdrftr-icon" src="/assets/icon_actionpoints.png"></img>
               </span>
               <span>
                 <h1 class="title is-size-2 has-text-white">5</h1>
@@ -31,7 +27,7 @@ const BaseWebsite = () => {
             <div class="level-item has-text-centered">
               <span>
                 <h1 class="heading has-text-centered has-text-white">Sparks</h1>
-                <img class="team" src="/assets/icon_sparks.png"></img>
+                <img class="hdrftr-icon" src="/assets/icon_sparks.png"></img>
               </span>
               <span>
                 <h1 class="title is-size-2 has-text-white">101</h1>
@@ -41,38 +37,37 @@ const BaseWebsite = () => {
         </div>
       </div>
       <div class="fixed-footer">
-        <div class="container">
-          <div class="notification has-background-dark">
-            <nav class="level is-mobile">
-              <div class="level-item has-text-centered">
-                <span>
-                  <h1 class="heading has-text-centered has-text-white">Round</h1>
-                  <img class="team" src="/assets/icon_day.png"></img>
-                </span>
-                <span>
-                  <h1 class="title is-size-2 has-text-white">1</h1>
-                </span>
+        <div class="notification has-background-dark">
+          <nav class="level is-mobile">
+            <div class="level-item has-text-centered">
+              <div class="badger has-background-dark" style="padding: 0px;">
+                <img class="hdrftr-icon" src="/assets/icon_chat.png"></img>
+                <h1 title="Badge top right" class="badge is-top-left">8</h1>
+                <h1 class="heading has-text-centered has-text-white">Chat</h1>
               </div>
-              <div class="level-item has-text-centered">
-                <span>
-                  <h1 class="heading has-text-centered has-text-white">Action Points</h1>
-                  <img class="team" src="/assets/icon_actionpoints.png"></img>
-                </span>
-                <span>
-                  <h1 class="title is-size-2 has-text-white">5</h1>
-                </span>
+            </div>
+            <div class="level-item has-text-centered">
+              <div class="badger has-background-dark" style="padding: 0px;">
+                <img class="hdrftr-icon" src="/assets/icon_vote.png"></img>
+                <h1 title="Badge top right" class="badge is-top-left">8</h1>
+                <h1 class="heading has-text-centered has-text-white">Vote</h1>
               </div>
-              <div class="level-item has-text-centered">
-                <span>
-                  <h1 class="heading has-text-centered has-text-white">Sparks</h1>
-                  <img class="team" src="/assets/icon_sparks.png"></img>
-                </span>
-                <span>
-                  <h1 class="title is-size-2 has-text-white">101</h1>
-                </span>
+            </div>
+            <div class="level-item has-text-centered">
+              <div class="badger has-background-dark" style="padding: 0px;">
+                <img class="hdrftr-icon" src="/assets/icon_camera.png"></img>
+                <h1 title="Badge top right" class="badge is-top-left">8</h1>
+                <h1 class="heading has-text-centered has-text-white">hHelp</h1>
               </div>
-            </nav>
-          </div>
+            </div>
+            <div class="level-item has-text-centered">
+              <div class="badger has-background-dark" style="padding: 0px;">
+                <img class="hdrftr-icon" src="/assets/icon_help.png"></img>
+                <h1 title="Badge top right" class="badge is-top-left">8</h1>
+                <h1 class="heading has-text-centered has-text-white">Settings</h1>
+              </div>
+            </div>
+          </nav>
         </div>
       </div>
     </div>
