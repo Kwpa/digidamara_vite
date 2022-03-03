763d0991cc455dad8b06a03da75cdd21367a09e9
Kwpa <kai.mcgilligan.oliver@gmail.com>
2022-03-03 10:19:38 -0400

    [Kai] Setup progress bars

--- src/scenes/elements/AvatarOverlay.jsx
+++ src/scenes/elements/AvatarOverlay.jsx
@@ -3,20 +3,22 @@ import { h } from 'start-dom-jsx';
 const AvatarOverlay = (avatarName) => {
   return (
     <div>
-      <div style="position: relative; width: 400px; height: 100vh; ">
-        <div style="position: absolute; bottom: 160px; width: inherit;">
-            <div class="level">
+      <div class="ov-container">
+        <div style="width: inherit;">
+          <div style="position: absolute; bottom: 20px; width: inherit;">
+            <div class="level box has-background-dark" style="padding: 0 0 0 0; border-style: solid; border-color: white;">
               <div class="level-item">
-              <img src="/assets/images/heart.png" width="50px" height="50px"></img>
-              <progress class="progress is-warning" value="30" max="100" style="background: #696969;">30%</progress>
+                <img src="/assets/images/heart.png" width="50px" height="50px"></img>
+                <progress id="teamEnergyBar" class="progress is-warning" value="30" max="100" style="background: #696969;">30%</progress>
               </div>
             </div>
             <div class="level">
               <div class="level-item">
-              <button id="openProfile" class="button is-primary">{avatarName}</button>
+                <button id="openProfile" class="button is-primary">{avatarName}</button>
               </div>
             </div>
-          
+
+          </div>
         </div>
       </div>
     </div>
